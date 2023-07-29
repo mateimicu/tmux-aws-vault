d0d546b5aac6fcf8f6a961d9ea21a7cfa5c1c82f
Matei <code@mateimicu.com>
2023-07-29 23:36:17 +0300

    fix: if aws envs are not present avoid showing 00h:00m

--- scripts/aws_remaining_time.sh
+++ scripts/aws_remaining_time.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
+AWS_EXPIRATION_TIME=${AWS_SESSION_EXPIRATION:-$AWS_CREDENTIAL_EXPIRATION}
 # based on https://stackoverflow.com/questions/63362712/ways-to-find-out-how-soon-the-aws-session-expires/72291361#72291361
 aws_session_time_left() {
   zulu_time_now=$1
 
-  aws_session_expiration_epoch="`date -j -u -f '%Y-%m-%dT%H:%M:%SZ' $AWS_SESSION_EXPIRATION '+%s'`"
+  aws_session_expiration_epoch="`date -j -u -f '%Y-%m-%dT%H:%M:%SZ' $AWS_EXPIRATION_TIME '+%s'`"
   zulu_time_now_epoch="`date -j -u -f '%Y-%m-%dT%H:%M:%SZ' $zulu_time_now '+%s'`"
 
-  if [[ $zulu_time_now < ${AWS_SESSION_EXPIRATION:-$AWS_CREDENTIAL_EXPIRATION} ]]; then
+  if [[ $zulu_time_now < $AWS_EXPIRATION_TIME ]]; then
     secs="`expr $aws_session_expiration_epoch - $zulu_time_now_epoch`"
     echo "+`printf '%dh:%02dm:%02ds\n' $((secs/3600)) $((secs%3600/60)) $((secs%60))`"
   else
@@ -16,6 +17,12 @@ aws_session_time_left() {
   fi
 }
 
-time_left=$(aws_session_time_left "`date -u +'%Y-%m-%dT%H:%M:%SZ'`")
+
+if [ -z "$AWS_EXPIRATION_TIME" ]; then
+    time_left=
+else
+  echo "AWS_EXPIRATION_TIME is set and not empty"
+    time_left=$(aws_session_time_left "`date -u +'%Y-%m-%dT%H:%M:%SZ'`")
+fi
 
 echo "$time_left"
